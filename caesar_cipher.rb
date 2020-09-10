52bfdfbd4be4fa5c3bba7613fb7be493da084110
Michael Mattson <michael.p.mattson@gmail.com>
2020-09-10 15:57:22 -0600

    added project

---
+++ caesar_cipher.rb
@@ -0,0 +1,39 @@
+def caesar word, shift_number
+  alphabet = ("a".."z").to_a
+  caesar_word = ''
+  up = false
+
+  word = word.split("")
+
+  word.each do |letter|
+    if letter == letter.upcase
+      up = true
+      letter = letter.downcase
+    end
+    
+    if alphabet.include?(letter)
+      alpha_index = alphabet.index(letter)
+      shifted_index = (alpha_index + shift_number) % 26
+      shifted_alpha = alphabet[shifted_index]
+    else
+      shifted_alpha = letter #if it's not in the alphabet, leave as is.
+    end
+
+    if up == true
+      shifted_alpha = shifted_alpha.upcase
+      up = false
+    end
+
+    caesar_word = caesar_word + shifted_alpha
+  end
+
+  word = word.join('')
+  puts "'#{word}' becomes: '#{caesar_word}'"
+end
+
+puts "Enter a string:"
+word = gets.chomp
+puts "Enter a number"
+shift_number = gets.chomp.to_i
+
+caesar word, shift_number
